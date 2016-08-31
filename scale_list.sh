6eff849820705244ca4a062cbd41140ca04d33b6
Richard L. Jackson <denezt@yahoo.com>
2016-08-31 19:43:57 +0200

    Initial Entry -- 2016-08-31

---
+++ scale_list.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+id=0
+option=$1
+
+
+view_list(){
+	filename=$1
+	__rows=($(wc -l $filename))
+	__records=$((${__rows[0]}-1))
+	__items=$((${__rows[0]}+1))
+
+	#Heading 
+	head -n 1 $filename	
+
+	for (( i=2; $i < ${__items[0]}; i++ ))
+	do
+		id=$((id+1))
+		printf "${id}|`head -n $i $filename | tail -n 1 | tr -d '\"' | tr ',' '|'`\n"
+	done
+	echo "Total Items [ ${__records[0]} ]"
+	}	
+
+
+case $option in
+	-v|-view|--view) 
+	if [ -e "$2" ];
+	then
+		view_list $2
+	else
+		printf "\033[35mError:\033[0m\tUnable to find filename \033[45m'$2'\033[0m\n" 
+	fi
+	;;
+	*) printf "\033[35mError:\033[0m\tMissing or invalid parameter was entered!\n";;
+esac
